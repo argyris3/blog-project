a97ae065525e05d0c1962f3a43f3752652a214ea
argyris3 <bigol3@hotmail.com>
2024-03-07 08:48:53 +0200

    dashcomments

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,9 +1,9 @@
-import { Button, Spinner } from "flowbite-react";
-import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Button, Spinner } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-import CommentSection from "../components/CommentsSection";
-import PostCard from "../components/POstCard";
+ 
+import CommentSection from '../components/CommentsSection';
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -59,27 +59,16 @@ export default function PostPage() {
     );
   return (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
-        {post && post.title}
-      </h1>
-      <Link
-        to={`/search?category=${post && post.category}`}
-        className="self-center mt-5"
-      >
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">{post && post.title}</h1>
+      <Link to={`/search?category=${post && post.category}`} className="self-center mt-5">
         <Button color="gray" pill size="xs">
           {post && post.category}
         </Button>
       </Link>
-      <img
-        src={post && post.image}
-        alt={post && post.title}
-        className="mt-10 p-3 max-h-[600px] w-full object-cover"
-      />
+      <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-cover" />
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className="italic">
-          {post && (post.content.length / 1000).toFixed(0)} mins read
-        </span>
+        <span className="italic">{post && (post.content.length / 1000).toFixed(0)} mins read</span>
       </div>
       <div
         className="p-3 max-w-2xl mx-auto w-full post-content"
@@ -91,8 +80,7 @@ export default function PostPage() {
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
         <div className="flex flex-wrap gap-5 mt-5 justify-center">
-          {recentPosts &&
-            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+          {recentPosts && recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
       </div>
     </main>
