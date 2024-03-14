9d609a69bd526792ecd8638190488260ced8992a
argyris3 <bigol3@hotmail.com>
2024-03-14 08:40:21 +0200

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,29 @@
 export default function About() {
-  return <div>about</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            Σχετικα με το Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Ενα διαφορετικο Blog που δημιουργηθηκε για να μοιραστει νεα απο
+              ολον τον κοσμο ειδησεις απο μια προσεχτικη ματια χωρις φιλτραρισμα
+              και ιδιοτελεια..
+            </p>
+            <p>
+              Σε αυτο το blog θα βρειτε ενδιαφεροντα νεα απο την μουσικη το
+              σινεμα τον αθλητισμο καθως και νεα απο ολον τον κοσμο..
+            </p>
+            <p>
+              Σας ενθαρυνουμε να αφησετε τα σχολια σας και αλληλοεπιδρασετε με
+              αλλους επισκεπτες..Μπορουμε σε αυτην την κοινοτητα να βοηθηθουμε
+              κανοντας καλυτερο ο ενας τον αλλον..{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
